d8d52fb612aaaaf681d7e9fbeaa5a28212c11a22
kingsley Diala <donbuski2@gmail.com>
2021-11-30 12:08:13 +0100

    initial commit

---
+++ src/Components/App/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+export class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+      <h2>Results</h2>
+      {/* <!-- Add a TrackList component --> */}
+    </div>
+    );
+  }
+}

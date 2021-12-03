17270c82fbdb67ff66087d24cbe3ea2f646b121d
kingsley Diala <donbuski2@gmail.com>
2021-12-03 22:57:58 +0100

    Change the Name of Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,10 +12,11 @@ class App extends React.Component {
       searchResults: [{name: 'name-1', artist: 'artist-1', album: 'album-1', id: 1}, {name: 'name-2', artist: 'artist-2', album: 'album-2', id: 2}, {name: 'name-3', artist: 'artist-3', album: 'album-3', id: 3}, {name: 'name-4', artist: 'artist-4', album: 'album-4', id: 4}, {name: 'name-5', artist: 'artist-5', album: 'album-5', id: 5}],
       PlaylistName: 'My Playlist',
       // Playlist Object
-      playlistTracks: [{name: 'Playlist-1', artist: 'PlaylistArtist-1', album: 'PlaylistAlbum-1', id: 6}, {name: 'Playlist-2', artist: 'PlaylistArtist-2', album: 'PlaylistAlbum-2', id: 7}, {name: 'Playlist-3', artist: 'PlaylistArtist-3', album: 'PlaylistAlbum-3', id: 8}, {name: 'Playlist-4', artist: 'PlaylistArtist-4', album: 'PlaylistAlbum-4', id: 9}, {Playlist: 'Playlist-5', artist: 'PlaylistArtist-5', album: 'PlaylistAlbum-5', id: 10}],
+      playlistTracks: [{name: 'Playlist-1', artist: 'PlaylistArtist-1', album: 'PlaylistAlbum-1', id: 6}, {name: 'Playlist-2', artist: 'PlaylistArtist-2', album: 'PlaylistAlbum-2', id: 7}, {name: 'Playlist-3', artist: 'PlaylistArtist-3', album: 'PlaylistAlbum-3', id: 8}, {name: 'Playlist-4', artist: 'PlaylistArtist-4', album: 'PlaylistAlbum-4', id: 9}, {name: 'Playlist-5', artist: 'PlaylistArtist-5', album: 'PlaylistAlbum-5', id: 10}],
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   // Accepts a track, uses the track id property to check it's state and add to playlist if new
@@ -35,6 +36,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: playlist })
   }
 
+  updatePlaylistName(name) {
+    this.setState({ PlaylistName: name });
+  }
+
 
   render() {
     return (
@@ -44,7 +49,7 @@ class App extends React.Component {
     <SearchBar />
     <div className="App-playlist">
       <SearchResults searchResult={ this.state.searchResults } onAdd={ this.addTrack } />
-      <Playlist playlist={ this.state.playlistTracks } removeTrack= { this.removeTrack }/>
+      <Playlist playlist={ this.state.playlistTracks } removeTrack= { this.removeTrack } onNameChange= { this.updatePlaylistName }/>
     </div>
   </div>
   </div>
--- src/Components/Playlist/playlist.js
+++ src/Components/Playlist/playlist.js
@@ -3,10 +3,24 @@
   import TrackList from "../TrackList/TrackList";
 
   export class Playlist extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+
+    // Handles the event on Playlist name change.
+    handleNameChange(event) {
+      this.props.onNameChange(event.target.value);
+    }
+
+
+
     render() {
       return (
         <div className="Playlist">
-        <input defaultValue={'New Playlist'}/>
+        <input defaultValue={'New Playlist'} onChange= { this.handleNameChange } />
         <TrackList tracks={ this.props.playlist } removeTrack= { this.props.removeTrack } isRemoval={true}/>
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
